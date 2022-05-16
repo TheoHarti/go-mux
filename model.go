01ede2c49859b00413fb902546b7329842ad66c0
TheoHarti <theo.hartleitner@gmail.com>
2022-05-16 19:31:33 +0200

    finished tutorial and added my own features

--- model.go
+++ model.go
@@ -2,31 +2,98 @@ package main
 
 import (
 	"database/sql"
-	"errors"
+	"math/rand"
 )
 
 type product struct {
-	ID    int     `json:"id"`
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
+    ID    int     `json:"id"`
+    Name  string  `json:"name"`
+    Price float64 `json:"price"`
 }
 
 func (p *product) getProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+    return db.QueryRow("SELECT name, price FROM products WHERE id=$1",
+        p.ID).Scan(&p.Name, &p.Price)
 }
 
 func (p *product) updateProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+    _, err :=
+        db.Exec("UPDATE products SET name=$1, price=$2 WHERE id=$3",
+            p.Name, p.Price, p.ID)
+
+    return err
 }
 
 func (p *product) deleteProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+    _, err := db.Exec("DELETE FROM products WHERE id=$1", p.ID)
+
+    return err
+}
+
+func (p *product) deleteAllProducts(db *sql.DB) error {
+    _, err := db.Exec("DELETE FROM products WHERE 1=1")
+
+    return err
 }
 
 func (p *product) createProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+    err := db.QueryRow(
+        "INSERT INTO products(name, price) VALUES($1, $2) RETURNING id",
+        p.Name, p.Price).Scan(&p.ID)
+
+    if err != nil {
+        return err
+    }
+
+    return nil
 }
 
-func getProducts(db *sql.DB, start, count int) ([]product, error) {
-	return nil, errors.New("Not implemented")
+func (p *product) createRandomProduct(db *sql.DB) error {
+	p.Name = generateRandomString(8)
+	p.Price = (rand.Float64() * 100)
+
+	err := db.QueryRow(
+        "INSERT INTO products(name, price) VALUES($1, $2) RETURNING id",
+        p.Name, p.Price).Scan(&p.ID)
+
+    if err != nil {
+        return err
+    }
+
+    return nil
+}
+
+func generateRandomString(n int) string {
+    var chars = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    str := make([]rune, n)
+
+    for i := range str {
+        str[i] = chars[rand.Intn(len(chars))]
+    }
+
+    return string(str)
+}
+
+func getProducts(db *sql.DB, start, count int, sortBy string) ([]product, error) {
+    rows, err := db.Query(
+        "SELECT id, name, price FROM products ORDER BY " + sortBy + " ASC LIMIT $1 OFFSET $2",
+        count, start)
+
+    if err != nil {
+        return nil, err
+    }
+
+    defer rows.Close()
+
+    products := []product{}
+
+    for rows.Next() {
+        var p product
+        if err := rows.Scan(&p.ID, &p.Name, &p.Price); err != nil {
+            return nil, err
+        }
+        products = append(products, p)
+    }
+
+    return products, nil
 }
