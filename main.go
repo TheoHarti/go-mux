7096e6bcf9182da436e780ec9689cd33d32ad8d1
TheoHarti <theo.hartleitner@gmail.com>
2022-05-26 22:22:32 +0200

    changed postgres to statically initialize variables

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
-import "os"
-
 func main() {
 	a := App{}
-	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+	a.Initialize("root", "pw", "db")
+//    a.Initialize(
+//        os.Getenv("APP_DB_USERNAME"),
+//        os.Getenv("APP_DB_PASSWORD"),
+//        os.Getenv("APP_DB_NAME"))
 
 	a.Run(":8010")
 }
--- main_test.go
+++ main_test.go
@@ -16,10 +16,11 @@ import (
 var a main.App
 
 func TestMain(m *testing.M) {
-    a.Initialize(
-        os.Getenv("APP_DB_USERNAME"),
-        os.Getenv("APP_DB_PASSWORD"),
-        os.Getenv("APP_DB_NAME"))
+    a.Initialize("root", "pw", "db")
+//    a.Initialize(
+//        os.Getenv("APP_DB_USERNAME"),
+//        os.Getenv("APP_DB_PASSWORD"),
+//        os.Getenv("APP_DB_NAME"))
 
     ensureTableExists()
     code := m.Run()
